aa1d277ccb96b9808a498a4cdee259202a2d8958
PhoebePactera <phoebe.tang@pactera.com>
2017-03-02 16:30:57 +0800

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,26 +1,220 @@
-testing phoebe
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hong Kong International Airport</title>
 
-require('../vendor/autoload.php');
+    <script src="jquery/jquery-3.1.1.min.js"></script>
+    <script src="jquery/jquery-ui-1.12.1.custom/jquery-ui.js"></script>
 
-$app = new Silex\Application();
-$app['debug'] = true;
+    <link rel="stylesheet" href="jquery/jquery-ui-1.12.1.custom/jquery-ui.css">
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
+    <!-- Bootstrap -->
+    <link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 
-// Our web handlers
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
 
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
+    <link rel="stylesheet" type="text/css" href="hkairport.css">
 
-$app->run();
+
+  </head>
+
+
+
+
+  <body>
+    <div class="page_container">
+      	<div class="home_upper">
+	        <img class="hkia_logo" src="img/logo.png" alt="">
+	        <div class="banner">
+		        <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+		            <div class="carousel-inner" role="listbox">
+		              <div class="carousel-item active">
+		                <img class="d-block img-fluid" src="img/banner/1.jpg" alt="First slide">
+		              </div>
+		              <div class="carousel-item">
+		                <img class="d-block img-fluid" src="img/banner/2.jpg" alt="Second slide">
+		              </div>
+		              <div class="carousel-item">
+		                <img class="d-block img-fluid" src="img/banner/3.jpg" alt="Third slide">
+		              </div>
+		            </div>
+		        </div>
+		    </div>
+
+	        <nav class="search_bar">
+	          <form class="form-inline">
+	            <input class="form-control" type="text" placeholder="Search">
+	            <button class="btn btn-default" type="submit">
+	              <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+	            </button>
+	          </form>
+	        </nav>
+
+	        <button type="button" class="m_navbar btn btn-default" aria-label="Left Align">
+			  <span class="glyphicon glyphicon-align-left" aria-hidden="true"></span>
+			</button>
+	    </div>
+
+	    <div class="navbar menu-container">
+	      <nav>
+	        <ul class="nav navbar-nav">
+	          <li class="dropdown">
+	            <a class="title dropdown-toggle" data-toggle="dropdown" href="#">flight info</a>
+	            <div class="dropdown-menu secondary-nav-container active">
+	              <a class="inspiration-promo" href="#">
+	                <img src="https://www.cathaypacific.com/content/dam/destinations/guilin/navigation-photo/guilin_fishermen_295x195.jpg">
+	              </a>
+	              <nav class="secondary-nav-links">
+	                <ul role="presentation">
+	                  <li role="presentation">
+	                    <a href="/cx/en_HK/destinations/things-to-do-in-hong-kong.html" role="menuitem">
+	                      <span class="link-header">Hong Kong</span>
+	                      <span class="link-additional-description">Hong Kong is the quintessential Asian city: full of contrasts, colour and excitement.
+	                      </span>
+	                    </a>
+	                  </li>
+	                  <li role="presentation">
+	                    <a href="/cx/en_HK/destinations/things-to-do-in-taipei.html" role="menuitem">
+	                      <span class="link-header">Taipei</span>
+	                      <span class="link-additional-description">Often overlooked in favour of its more famous Asian cousins, the tiny island of Taiwan is just waiting to be discovered.</span>
+	                    </a>
+	                  </li>
+	                  <li role="presentation">
+	                    <a href="/cx/en_HK/destinations/things-to-do-in-singapore.html" role="menuitem">
+	                      <span class="link-header">Singapore</span>
+	                      <span class="link-additional-description">Once a sleepy fishing village, Singapore has blossomed into an industrial powerhouse with a diverse and surprising character.</span>
+	                   </a>
+	                  </li>
+	                  <li role="presentation">
+	                    <a href="/cx/en_HK/destinations/things-to-do-in-osaka.html" role="menuitem">
+	                      <span class="link-header">Osaka</span>
+	                      <span class="link-additional-description">Osaka is Japan's third largest city and home to some of the country's best cultural sites.
+	                      </span>
+	                    </a>
+	                  </li>
+	                </ul>
+	              </nav>
+	            </div>
+	          </li>
+	        </ul>
+	      </nav>
+	    </div>
+
+
+
+	    <div class="row content">
+	        <div class="col-xs-12">
+	          	<h3 class="title navy">flight information</h3>
+	          	<div class="flight_info_tab">
+	            	<div class="title navy">passenger</div>
+	            	<div class="title navy">cargo</div>
+	          	</div>
+
+	        	<div class="flight_info_container">
+	          		<div class="flight_info_bounding">
+			            <span class="title navy">
+			            	<span class="glyphicon glyphicon-plane" aria-hidden="true"></span>Arrivals
+			            </span>
+			            <span class="title navy">
+			            	<span class="glyphicon glyphicon-plane" aria-hidden="true"></span>Departures
+			            </span>
+			        </div>
+			        <div class="flight_info_form">
+			        	<div>
+							<div class="flight_info_input form-group">
+							  <select class="form-control" id="destination">
+							    <option>All</option>
+							    <option>Hong Kong</option>
+							    <option>New York</option>
+							    <option>Tokyo</option>
+							  </select>
+							</div>
+							<div class="flight_info_input form-group">
+							  <select class="form-control" id="airline">
+							    <option>Airline</option>
+							    <option>Cathay Pacific</option>
+							    <option>Hong Kong Airline</option>
+							    <option>Peach Airline</option>
+							  </select>
+							</div>
+						</div>
+						<div class="float_left">
+							<div class="flight_info_input form-group">
+							  <select class="form-control" id="date">
+							    <option>2 Mar 2017</option>
+							    <option>3 Mar 2017</option>
+							    <option>4 Mar 2017</option>
+							    <option>5 Mar 2017</option>
+							  </select>
+							</div>
+							<div class="float_left">
+								<div class="flight_info_input form-group">
+								  <input type="email" class="form-control" id="flight_num" aria-describedby="flight_num" placeholder="Flight Number">
+								</div>
+								<div class="flight_info_input glyphicon glyphicon-arrow-right" aria-hidden="true"></div>
+							</div>
+						</div>
+					</div>
+		        </div>
+	         	<div class="place_info">
+	         		<img src="img/hk.jpg">
+	         		<div class="place_detail">Hong Kong</div>
+	         	</div>
+		    </div>
+	        <div class="col-xs-12 col-sm-4"">
+	          <h3 class="title title2">passenger guide</h3>
+	          <img src="img/content/part2a.jpg">
+	          <h3 class="title title3">to & from airport</h3>
+	          <img src="img/content/part2b.jpg">
+	        </div>
+	        <div class="col-xs-12 col-sm-4">
+	          <h3 class="title title4">shopping, dinning & entertainment</h3>
+	          <img src="img/content/part3.jpg">
+	        </div>
+	        <div class="col-xs-12 col-sm-4">
+	          <h3 class="title title5">our business</h3>
+	          We are making every effort to provide passengers with an excellent airport experience.
+	          <img src="img/content/part4.jpg">
+	        </div>
+	    </div>
+
+
+
+	    <div class="row home_gif_banner">
+	        <div class="col-xs-12 col-sm-4">
+	          <img src="img/gif/gif1.gif">
+	        </div>
+	        <div class="col-xs-12 col-sm-4">
+	          <img src="img/gif/gif2.gif">
+	        </div>
+	        <div class="col-xs-12 col-sm-4">
+	          <img src="img/gif/gif3.gif">
+	        </div>
+	    </div>
+	</div>
+
+    <div class="footer">
+      <div class="page_container">
+        <div id="footerLeft"><a onclick="javascript:trackPage('footer', this);" href="/eng/business/airport-authority/career/index.html" target="_top">Careers</a> | <a onclick="javascript:trackPage('footer', this);" href="/eng/others/sitemap.html" target="_top">Sitemap</a> | <a onclick="javascript:trackPage('footer',this);" href="/eng/others/access_to_information.html" target="_top">Access To Information</a> | <a onclick="javascript:trackPage('footer',this);" href="/eng/others/privacy_policy.html" target="_top">Privacy Policy</a> | <a onclick="javascript:trackPage('footer', this);" href="/eng/others/disclaimer.html" target="_top">Disclaimer</a> | <a onclick="javascript:trackPage('footer', this);" href="/eng/others/terms_of_use.html" target="_top">Terms Of Use</a> | <a onclick="javascript:trackPage('footer', this);" href="/eng/others/accessibility_statement.html" target="_top">Accessibility Statement</a><br>© Airport Authority Hong Kong. All Rights Reserved.</div>
+      </div>
+    </div>
+  </body>
+</html>
